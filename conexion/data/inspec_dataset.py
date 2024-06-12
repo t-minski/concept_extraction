66975250202f9214a7cca0329e49d557670f3ac4
ebrahim <ebrahim.norouzi@rub.de>
2024-06-12 09:02:15 +0000

    check to only include the extractive keyphrases

--- conexion/data/inspec_dataset.py
+++ conexion/data/inspec_dataset.py
@@ -6,8 +6,16 @@ from datasets import load_dataset
 class inspec(BaseDataset):                
     def get_training_data(self) -> Tuple[List[str], List[List[str]]]:
         training_dataset = load_dataset("midas/inspec", "raw")["train"]
-        return [' '.join(tokens) for tokens in training_dataset["document"]], training_dataset["extractive_keyphrases"]
+        filtered_documents = [' '.join(tokens) for tokens in training_dataset["document"]]
+        filtered_keyphrases = [[phrase for phrase in keyphrases if phrase in ' '.join(tokens)]
+                            for tokens, keyphrases in zip(training_dataset["document"], training_dataset["extractive_keyphrases"])]
+
+        return filtered_documents, filtered_keyphrases
         
     def get_test_data(self) -> Tuple[List[str], List[List[str]]]:
         test_dataset = load_dataset("midas/inspec", "raw")["test"]
-        return [' '.join(tokens) for tokens in test_dataset["document"]], test_dataset["extractive_keyphrases"]
+        filtered_documents = [' '.join(tokens) for tokens in test_dataset["document"]]
+        filtered_keyphrases = [[phrase for phrase in keyphrases if phrase in ' '.join(tokens)]
+                            for tokens, keyphrases in zip(test_dataset["document"], test_dataset["extractive_keyphrases"])]
+
+        return filtered_documents, filtered_keyphrases
