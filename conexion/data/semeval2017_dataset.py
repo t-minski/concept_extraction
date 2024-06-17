e3e3bc9afff76e5c70be01d2f607e4862e01c18f
ebrahim <ebrahim.norouzi@rub.de>
2024-06-17 13:18:40 +0000

    update other datasets

--- conexion/data/semeval2017_dataset.py
+++ conexion/data/semeval2017_dataset.py
@@ -5,7 +5,7 @@ from datasets import load_dataset
 
 class semeval2017(BaseDataset):                
     def get_training_data(self) -> Tuple[List[str], List[List[str]]]:
-        training_dataset = load_dataset("midas/semeval2017", "raw")["train"]
+        training_dataset = load_dataset("midas/semeval2017", "generation", split="train", revision="d0e6006", trust_remote_code=True)
         filtered_documents, filtered_keyphrases = log_statistics(
                                                                     training_dataset["document"],
                                                                     training_dataset["extractive_keyphrases"]
@@ -14,7 +14,7 @@ class semeval2017(BaseDataset):
         return filtered_documents, filtered_keyphrases
         
     def get_test_data(self) -> Tuple[List[str], List[List[str]]]:
-        test_dataset = load_dataset("midas/semeval2017", "raw")["test"]
+        test_dataset = load_dataset("midas/semeval2017", "generation", split="test", revision="d0e6006", trust_remote_code=True)
         filtered_documents, filtered_keyphrases = log_statistics(
                                                                     test_dataset["document"],
                                                                     test_dataset["extractive_keyphrases"]
