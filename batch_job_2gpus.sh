fbbe482c4aa4124182f0d2d056bcd4a14bf23601
Sven Hertling <svenhertling@t-online.de>
2024-07-09 14:55:54 +0200

    batch job update

--- batch_job_2gpus.sh
+++ batch_job_2gpus.sh
@@ -5,5 +5,6 @@
 #SBATCH --gres=gpu:A100:2
 
 export HF_HOME=/gpfs/bwfor/work/ws/ma_shertlin-conexion/hf_home
+conda activate conexion
 log_file="./output/${1////*}_${3}_${2}.log"
 python main.py --models class=LLMBaseModel,prompt=${2},model_name=${1},with_confidence=False,batched_generation=False --datasets ${3} --output ./output -v > ${log_file} 2>&1
