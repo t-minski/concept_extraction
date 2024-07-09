17117d9b1dd656c1d6a41c0e577cd1c46bf1eff7
Sven Hertling <svenhertling@t-online.de>
2024-07-09 19:42:43 +0200

    update conda in batch jobs

--- batch_job_2gpus.sh
+++ batch_job_2gpus.sh
@@ -4,7 +4,9 @@
 #SBATCH --time=12:00:00
 #SBATCH --gres=gpu:A100:2
 
-export HF_HOME=/gpfs/bwfor/work/ws/ma_shertlin-conexion/hf_home
+module load devel/miniconda/3
 conda activate conexion
+
+export HF_HOME=/gpfs/bwfor/work/ws/ma_shertlin-conexion/hf_home
 log_file="./output/${1////*}_${3}_${2}.log"
 python main.py --models class=LLMBaseModel,prompt=${2},model_name=${1},with_confidence=False,batched_generation=False --datasets ${3} --output ./output -v > ${log_file} 2>&1
