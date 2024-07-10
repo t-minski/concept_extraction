9ced461762b0c22cfae3f32c37dd9afb47d11485
ebrahim <ebrahim.norouzi@rub.de>
2024-07-10 06:47:29 +0000

    solved an error in stemmer

--- conexion/evaluation/evaluator.py
+++ conexion/evaluation/evaluator.py
@@ -117,9 +117,9 @@ def evaluate_transfer_learning(models : List[BaseModel], datasets : List[Tuple[B
                     prf_5, prf_10, prf_15, ndcg, map = evaluate_p_r_f_at_k(predicted_concepts_with_confidence[i], test_concepts[i])
                     
                     # tokenization and stemming
-                    stemmed_only_keyword = [[" ".join([PorterStemmer().stem(tok.text.lower()) for tok in nlp(keyphrase)]) for keyphrase in sample] for sample in only_keyword ]
-                    stemmed_test = [[" ".join([PorterStemmer().stem(tok.text.lower()) for tok in nlp(keyphrase)]) for keyphrase in sample] for sample in test_concepts[i] ]
-                    stemmed_predicted_concepts_with_confidence = [[" ".join([PorterStemmer().stem(tok.text.lower()) for tok in nlp(keyphrase)]) for keyphrase in sample] for sample in predicted_concepts_with_confidence[i] ]
+                    stemmed_only_keyword = [[" ".join([PorterStemmer().stem(tok.text.lower()) for tok in nlp(keyphrase)]) for keyphrase in only_keyword ]]
+                    stemmed_test = [[" ".join([PorterStemmer().stem(tok.text.lower()) for tok in nlp(keyphrase)]) for keyphrase in test_concepts[i] ]]
+                    stemmed_predicted_concepts_with_confidence = [[" ".join([PorterStemmer().stem(tok.text.lower()) for tok in nlp(keyphrase)]) for keyphrase in predicted_concepts_with_confidence[i] ]]
                     stemmed_precision, stemmed_recall, stemmed_f1_score = evaluate_p_r_f(stemmed_only_keyword, stemmed_test)
                     stemmed_prf_5, stemmed_prf_10, stemmed_prf_15, stemmed_ndcg, stemmed_map = evaluate_p_r_f_at_k(stemmed_predicted_concepts_with_confidence, stemmed_test)
                     
