40fcce046bd7d0eeb0bf1140a6bb9f47e9d19b72
Sven Hertling <svenhertling@t-online.de>
2024-07-11 10:36:48 +0200

    batched files

---
+++ run_fs_fixed_LLM-batch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define templates
+class_models=(
+    "LLMRandomTraining"
+    "LLMRandomButFixedTraining"
+    "LLMClosestTraining"
+)
+
+# Define numbers
+numbers=(
+    "1"
+    "3"
+    "5"
+)
+
+
+for class_model in "${class_models[@]}"
+do
+	for number in "${numbers[@]}"
+	do
+		# Run the command and log the output, continue to next command even if there is an error
+		sbatch --job-name "fs-${class_model}-${number}" run_fs_fixed_LLM-job.sh ${class_model} ${number}
+	done
+done
---
+++ run_fs_fixed_LLM-job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=single
+#SBATCH --ntasks=15
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:A100:2
+
+source activate conexion
+
+log_file="./output/fs_fixed_Meta-Llama-3-70B-Instruct_${1}_${2}.log"
+python main.py --models class=${1},prompt=fs_keyphrases,model_name=meta-llama/Meta-Llama-3-70B-Instruct,number_of_examples=${2},load_in_4bit=True --datasets inspec --output ./output -v > ${log_file} 2>&1
